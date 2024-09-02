25baec2bf35ab2ec23417ffabc5ab996c498dcd5
ella4719 <rhdmstn0424@gmail.com>
2024-09-02 22:18:41 +0900

    💬models에 백업 코드 입력

--- models/index.js
+++ models/index.js
@@ -1,4 +1,5 @@
 const Sequelize = require('sequelize');
+const sequelize = require('../config/database');
 
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
@@ -6,7 +7,26 @@ const db = {};
 
 const sequelize = new Sequelize(config.database, config.username, config.password, config);
 
-
 db.sequelize = sequelize;
+
+const Sharer = require('./Sharer');
+const Itinerary = require('./Itinerary');
+const Place = require('./Place');
+// 모델 간의 관계 설정
+Itinerary.hasMany(Place, { foreignKey: 'itinerary_id', onDelete: 'CASCADE' });
+Place.belongsTo(Itinerary, { foreignKey: 'itinerary_id' });
+
+Itinerary.hasMany(Sharer, { foreignKey: 'itinerary_id', onDelete: 'CASCADE' });
+Sharer.belongsTo(Itinerary, { foreignKey: 'itinerary_id' });
+
+// 모델들을 내보내기
+const db = {
+    Sequelize,
+    sequelize,
+    Itinerary,
+    Place,
+    Sharer
+};
+
 
 module.exports = db;
