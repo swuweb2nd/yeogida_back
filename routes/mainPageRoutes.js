9ed09af8812e9034f45b5a36de5059e9d5904a15
ella4719 <rhdmstn0424@gmail.com>
2024-10-14 21:25:57 +0900

    api 연결 오류 수정

--- routes/mainPageRoutes.js
+++ routes/mainPageRoutes.js
@@ -3,12 +3,12 @@ const router = express.Router();
 const mainPageController = require('../controllers/mainPageController');
 
 // 1. 7일간의 게시물 조회 (인기순 및 최신순)
-router.get('/itineraries/recent', mainPageController.getRecentItineraries);
+router.get('/recent', mainPageController.getRecentItineraries);
 
 // 2. 가장 많이 여행한 도시 Top 5 조회
-router.get('/itineraries/top-destinations', mainPageController.getTopDestinations);
+router.get('/top-destinations', mainPageController.getTopDestinations);
 
 // 3. 일정 검색 기능 (제목, 목적지, 설명에서 검색)
-router.get('/itineraries/search', mainPageController.searchItineraries);
+router.get('/search', mainPageController.searchItineraries);
 
 module.exports = router;
