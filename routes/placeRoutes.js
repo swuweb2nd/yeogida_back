d172df146b144085cb7662744761e789f9c40925
ella4719 <rhdmstn0424@gmail.com>
2024-11-08 23:50:54 +0900

    isLoggedIn, isNotLoggedIn - mainPage, placeRoutes쪽에 추가

--- routes/placeRoutes.js
+++ routes/placeRoutes.js
@@ -1,24 +1,26 @@
 const express = require('express');
 const router = express.Router();
 const placeController = require('../controllers/placeController');
+const { isLoggedIn, isNotLoggedIn} = require('../middlewares');
+
 
 // 장소 검색 API 추가 (고유한 경로로 설정)
-router.get('/places/search', placeController.searchPlaces);
+router.get('/places/search', isLoggedIn, placeController.searchPlaces);
 
 // 특정 여행일정에 대한 모든 여행장소 조회
-router.get('/:itinerary_id/places', placeController.getPlacesByItineraryId);
+router.get('/:itinerary_id/places',isLoggedIn, placeController.getPlacesByItineraryId);
 
 // 특정 여행일정에 새로운 여행장소 추가
-router.post('/:itinerary_id/places', placeController.createPlace);
+router.post('/:itinerary_id/places', isLoggedIn, placeController.createPlace);
 
 // 특정 여행장소 조회
-router.get('/places/:place_id', placeController.getPlaceById);
+router.get('/places/:place_id',isLoggedIn, placeController.getPlaceById);
 
 // 특정 여행장소 수정
-router.put('/places/:place_id', placeController.updatePlace);
+router.put('/places/:place_id',isLoggedIn, placeController.updatePlace);
 
 // 특정 여행장소 삭제
-router.delete('/places/:place_id', placeController.deletePlace);
+router.delete('/places/:place_id',isLoggedIn, placeController.deletePlace);
 
 
 module.exports = router;
